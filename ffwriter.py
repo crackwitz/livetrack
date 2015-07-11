66c3381ee0475650031d0d02874fad5dc407c407
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2015-07-12 00:47:25 +0200

    ffwriter: pipe video to ffmpeg for encoding

---
+++ ffwriter.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2
+import subprocess
+
+class NullFile(object):
+	def fileno(self):
+		return 0
+
+	def write(self, data):
+		pass
+
+class FFWriter(object):
+	def __init__(self, fname, fps, (width, height), pixfmt=None, moreflags=''):
+		self.width = width
+		self.height = height
+
+		self.proc = subprocess.Popen([
+			"ffmpeg",
+			#'-loglevel', '32',
+			'-f', 'rawvideo',
+			'-pix_fmt', 'bgr24',
+			'-s', '{0}x{1}'.format(width, height),
+			'-r', '{0}'.format(fps),
+			'-i', 'pipe:0',
+		] + ([
+			'-pix_fmt', pixfmt
+			#'-filter:v', 'format=pix_fmts={0}'.format(pixfmt or '')
+		] * (pixfmt is not None)) + [
+			'-c:v', 'libx264'
+		] + (moreflags.split() if isinstance(moreflags, str) else moreflags) + [
+			'-y',
+			fname
+		], stdin=subprocess.PIPE) #, stderr=NullFile())
+	
+	def isOpened(self):
+		return True
+	
+	def write(self, frame):
+		assert frame.dtype == np.uint8
+		assert frame.shape[2] == 3
+		assert frame.shape[0] == self.height
+		assert frame.shape[1] == self.width
+		frame.tofile(self.proc.stdin)
+	
+	def close(self):
+		self.proc.stdin.close()
+		return self.proc.wait()
+
+	def __del__(self):
+		self.close()
+
+
+if __name__ == '__main__':
+	#frame = np.zeros((1080, 1920,3), dtype=np.uint8)
+
+	cam = cv2.VideoCapture(0)
+	rv,frame = cam.read()
+
+	# https://www.ffmpeg.org/ffmpeg-codecs.html#libx264_002c-libx264rgb
+	
+	vid = FFWriter("test.mov", 25, frame.shape[1::-1], '-crf 15 -preset ultrafast')
+	vid.write(frame)
+
+	try:
+		while True:
+			rv,frame = cam.read()
+			vid.write(frame)
+
+	finally:
+		vid.close()
+	
+	
